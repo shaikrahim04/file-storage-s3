8f103d589b2e0fbc55ef5b102dc1bdade9bedecf
shaikrahim04 <theshaikrahim@gmail.com>
2025-07-20 08:14:03 +0000

    feat: Generate Presigned URL's with expiration time

--- src/api/video-meta.ts
+++ src/api/video-meta.ts
@@ -4,6 +4,7 @@ import { createVideo, deleteVideo, getVideo, getVideos } from "../db/videos";
 import { respondWithJSON } from "./json";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 import type { BunRequest } from "bun";
+import { dbVideoToSignedVideo } from "./videos.ts"
 
 export async function handlerVideoMetaCreate(cfg: ApiConfig, req: Request) {
   const token = getBearerToken(req.headers);
@@ -54,8 +55,9 @@ export async function handlerVideoGet(cfg: ApiConfig, req: BunRequest) {
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
+  const signedVideo = dbVideoToSignedVideo(cfg, video);
 
-  return respondWithJSON(200, video);
+  return respondWithJSON(200, signedVideo);
 }
 
 export async function handlerVideosRetrieve(cfg: ApiConfig, req: Request) {
