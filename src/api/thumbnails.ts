91a56ef9f42b35304ee9af8ad5f44fa6c9e75f6d
shaikrahim04 <theshaikrahim@gmail.com>
2025-07-24 07:45:58 +0000

    Reapply "feat: Establish s3 uploads and thumbnail handling"
    This reverts commit a5365a75a8a30413db7fe895237fcb730c0916ad.

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import { randomBytes } from "crypto";
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -45,9 +16,49 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
+
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`,
+    );
+  }
+
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  if(mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("Invalid Content-Type for thumbnail");
+  }
+   
+  const ext = mediaTypeToExt(mediaType);
+
+  const randomFileName = randomBytes(32).toString('base64');
+  // console.log(randomFileName);
+  const filename = `${randomFileName}${ext}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
+
+  const urlPath = getAssetURL(cfg, filename);
+  video.thumbnailURL = urlPath;
+  updateVideo(cfg.db, video);
 
   return respondWithJSON(200, null);
 }
--- src/api/videos.ts
+++ src/api/videos.ts
@@ -3,6 +3,178 @@ import { respondWithJSON } from "./json";
 import { type ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 
+import { BadRequestError, UserForbiddenError, NotFoundError } from "./errors";
+import { getBearerToken, validateJWT } from "../auth";
+import { getVideo, updateVideo } from "../db/videos";
+import { getAssetDiskPath, mediaTypeToExt } from "./assets";
+import { s3, S3Client } from "bun";
+import { randomBytes } from "crypto";
+
 export async function handlerUploadVideo(cfg: ApiConfig, req: BunRequest) {
+
+  const MAX_UPLOAD_SIZE = 1 << 30;
+  
+  const { videoId } = req.params as { videoId?: string };
+
+  if (!videoId) {
+    throw new BadRequestError("Invalid video ID");
+  }
+
+  const token = getBearerToken(req.headers);
+  const userID = validateJWT(token, cfg.jwtSecret);
+
+  const video = getVideo(cfg.db, videoId);
+
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
+
+
+
+
+  const formData = await req.formData();
+  const file = formData.get("video");
+
+  if ( !(file instanceof Blob ) ){
+    throw new  BadRequestError("Video File is missing");
+  }
+
+  if ( file.size > MAX_UPLOAD_SIZE ) {
+    throw new BadRequestError("Vide File exceeds max allowed size of 1GB");
+  }
+
+  const mediaType = file.type;
+
+  if (!mediaType) {
+    throw new BadRequestError("Missing media type for video");
+  }
+
+  if ( mediaType != "video/mp4" ) {
+    throw new BadRequestError("Invalid media type for video");
+  }
+
+  const ext = mediaTypeToExt(mediaType);
+  const randomFileName = randomBytes(32).toString('hex');
+  
+  const fileName = `${randomFileName}.${ext}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, fileName);
+
+  await Bun.write(assetDiskPath, file);
+
+  const assetDiskPathProcessed = await processVideoForFastStart(assetDiskPath);
+  await Bun.file(assetDiskPath).delete()
+
+  const aspectSize = await getVideoAspectRatio(assetDiskPathProcessed);
+
+  const s3File = cfg.s3Client.file(`${aspectSize}/${fileName}`, { type: mediaType });
+  await s3File.write(Bun.file(assetDiskPathProcessed));
+
+  await Bun.file(assetDiskPathProcessed).delete();
+
+  video.videoURL = `https://${cfg.s3Bucket}.s3.${cfg.s3Region}.amazonaws.com/${aspectSize}/${fileName}`;
+  updateVideo(cfg.db, video);
+  
+
+
   return respondWithJSON(200, null);
+}
+
+
+async function getVideoAspectRatio(filePath: string) {
+  const proc = Bun.spawn({
+    cmd: [
+      "ffprobe",
+      "-v",
+      "error",
+      "-select_streams",
+      "v:0",
+      "-show_entries",
+      "stream=width,height",
+      "-of",
+      "json",
+      filePath
+    ],
+    stdout: "pipe",
+    stderr: "pipe"
+  });
+
+  const stdout = await Bun.readableStreamToText(proc.stdout);
+  const stderr = proc.stderr ? await Bun.readableStreamToText(proc.stderr) : "";
+
+  const  exitCode  = await proc.exited;
+  if (exitCode !== 0) {
+    throw new Error(`ffprobe error (exit code ${exitCode}): ${stderr}`);
+  }
+
+  try {
+    const data = JSON.parse(stdout);
+    const stream = data.streams && data.streams[0];
+
+    if (stream && typeof stream.width === 'number' && typeof stream.height === 'number') {
+      const width = stream.width;
+      const height = stream.height;
+
+      // Only needed if sending the reduced ratios
+
+      // const gcd = (a: number, b: number): number => b === 0 ? a : gcd(b, a % b);
+
+      // const divisor = gcd(width, height);
+      // const w = Math.round(width / divisor);
+      // const h = Math.round(height / divisor);
+
+      const tolerance = 0.01;
+
+      const actualRatio = width / height;
+
+      if (Math.abs(actualRatio - (16 / 9)) < tolerance) {
+        return "landscape";
+      }
+      else if (Math.abs(actualRatio - (9 / 16)) < tolerance) {
+        return "portrait";
+      }
+      else {
+        return `other`;
+      }
+    }
+    throw new Error("Width or height not found or invalid in ffprobe output.");
+  } catch (e: any) {
+    throw new Error(`Failed to parse ffprobe output: ${e.message || e}`);
+  }
+}
+async function processVideoForFastStart(inputFilePath: string) {
+
+
+  const outputFilePath = inputFilePath + ".processed";
+
+  const proc = Bun.spawn({
+    cmd: [
+      "ffmpeg",
+      "-i",
+      inputFilePath,
+      "-movflags",
+      "faststart",
+      "-map_metadata",
+      "0",
+      "-codec",
+      "copy",
+      "-f",
+      "mp4",
+      outputFilePath
+    ],
+    stdout: "pipe",
+    stderr: "pipe"
+  });
+
+  const exitCode = await proc.exited;
+  if (exitCode !== 0) {
+    const stderr = proc.stderr ? await Bun.readableStreamToText(proc.stderr) : "";
+    throw new Error(`ffmpeg error (exit code ${exitCode}): ${stderr}`);
+  }
+
+  return outputFilePath;
 }
